25727d20acc569d15dfd2df7942352900c89f2f3
David Govea <govea.d@gmail.com>
2020-09-20 20:00:20 -0700

    test: add babel module resolution for local-file testing
    Follows pattern from react-native-gesture-handler

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          'react-native-dynamic-vector-icons': '../',
+          react: './node_modules/react',
+          'react-native-vector-icons':
+            './node_modules/react-native-vector-icons',
+        },
+      },
+    ],
+  ],
 };
